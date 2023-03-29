0b028e508d4c073913da0dfc446e5de1494f1317
iuli <iulia.stoichescu25@gmail.com>
2023-03-29 21:43:58 +0300

    SM-1 Added the login screen

--- src/main/java/com/example/studentmanager/HelloApplication.java
+++ src/main/java/com/example/studentmanager/HelloApplication.java
@@ -9,13 +9,13 @@ import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("fxml/login.fxml"));
+        primaryStage.setTitle("FIS - Student Manager");
+        primaryStage.setScene(new Scene(root, 600, 400));
+        primaryStage.show();
     }
+
 
     public static void main(String[] args) {
         launch();
